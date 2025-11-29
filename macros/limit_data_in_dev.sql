0f80a4c829ff2477aa479ae7da084b4d84ae2ee9
puttu.gowda@gmail.com <puttu.gowda@gmail.com>
2025-11-29 12:58:21 +0000

    created macros for calculation

---
+++ macros/limit_data_in_dev.sql
@@ -0,0 +1,10 @@
+{{% limit_data_in_dev(column_name) %}}
+
+{% if target.name == 'development' %}
+
+where {{column_name}} >= dateadd('day', -30, current_timestamp)
+
+{% endif %}
+
+
+{{% endmacro %}}
